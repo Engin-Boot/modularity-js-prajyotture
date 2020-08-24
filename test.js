4ae4c4c0fd45e761918d78a842fcd2e66ba080a8
Prajyot Ture <47532830+prajyotture@users.noreply.github.com>
2020-08-24 23:04:28 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,16 @@
+
+
+function testNumberToPair(number, expectedMajor, expectedMinor) {
+    const pairOfColors = GetColorFromPairNumber(number);
+    console.log(`${number} = ${expectedMajor} ${expectedMinor}`);
+    expect(pairOfColors.major).equals(expectedMajor);
+    expect(pairOfColors.minor).equals(expectedMinor);
+}
+
+function testColorToNumber(majorColor, minorColor, expectedNumber) {
+    const pairNumber = GetPairNumberFromColors(majorColor, minorColor);
+    console.log(`${majorColor} ${minorColor} = ${pairNumber}`);
+    expect(pairNumber).to.equal(expectedNumber);
+}
+module.export=testNumberToPair;
+module.export=testNumberToPair;
